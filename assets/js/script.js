533a84ba7bd3683ca891abbe5b6855ed64744d06
StefanoCeli <stefanopsn91@gmail.com>
2021-04-22 23:22:17 +0200

    Aggiunto stile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     //creo un array e inserisco delle stringhe
     var arrToDo=[
-        "chiamare il medico",
-        "fare la spesa",
-        "registrare programma preferito",
+        "Chiamare il medico",
+        "Fare la spesa",
+        "Registrare programma preferito",
         "Prenotare gioco in uscita"
     ]
     //creo un ciclo per stampare a video l'array
     for(var i=0; i<arrToDo.length; i++){
         var ToDo=arrToDo[i]
         //console.log(ToDo);
-        var strToDo= '<li>' + ToDo + '<i class="far fa-trash-alt"></i></li>';
+        var strToDo= '<li><p>' + ToDo + '</p><i class="far fa-trash-alt"></i></li>';
         $('.container ul').append(strToDo)
     }
     //qui tramite la funzione "on" vado a richiamare l'evento click che fa riferimento all'icona e gli vado a dire che una volta cliccato di rimuovere il padre
@@ -22,7 +22,7 @@ $(document).ready(function(){
 
         if(event.which===13){
             //console.log($(this).val());
-            var strToDo= '<li>' + $(this).val().trim() + '<i class="far fa-trash-alt"></i></li>';
+            var strToDo= '<li><p>' + $(this).val().trim() + '</p><i class="far fa-trash-alt"></i></li>';
             //qui vado a specificare che se la parola è piu lunga di 3 lettere,vado a inserire la stringa dentro l'ul,dopodichè vado a svuotare l'input dicendogli di sovrascrivere il valore con una stringa vuota
             if($(this).val().length>3){
                 $('.container ul').append(strToDo);
